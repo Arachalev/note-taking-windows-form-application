bfc973868d484e17fd41a93a9da74d7a65f9b53b
Arachalev <ekongedet01@gmail.com>
2023-02-03 13:50:19 +0100

    modified appearance and delete functionality

---
+++ NoteApp.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace note_taking_app
+{
+    partial class NoteApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.MessageStore = new System.Windows.Forms.DataGridView();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MessageStore)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(50, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Snow;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(50, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(140, 61);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 22);
+            this.txtTitle.TabIndex = 2;
+            this.txtTitle.TextChanged += new System.EventHandler(this.txtTitle_TextChanged);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(140, 142);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(116, 141);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // MessageStore
+            // 
+            this.MessageStore.AllowUserToAddRows = false;
+            this.MessageStore.AllowUserToDeleteRows = false;
+            this.MessageStore.AllowUserToResizeColumns = false;
+            this.MessageStore.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MessageStore.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MessageStore.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MessageStore.Location = new System.Drawing.Point(333, 61);
+            this.MessageStore.Name = "MessageStore";
+            this.MessageStore.ReadOnly = true;
+            this.MessageStore.RowHeadersVisible = false;
+            this.MessageStore.RowHeadersWidth = 51;
+            this.MessageStore.RowTemplate.Height = 24;
+            this.MessageStore.Size = new System.Drawing.Size(280, 222);
+            this.MessageStore.TabIndex = 4;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNew.Location = new System.Drawing.Point(53, 341);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(62, 25);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.Location = new System.Drawing.Point(158, 341);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(62, 25);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.Location = new System.Drawing.Point(396, 341);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(62, 25);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRead.Location = new System.Drawing.Point(291, 341);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(62, 25);
+            this.btnRead.TabIndex = 7;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // NoteApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.MessageStore);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "NoteApp";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MessageStore)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.DataGridView MessageStore;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ NoteApp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace note_taking_app
+{
+    public partial class NoteApp : Form
+    {
+
+        DataTable dataTable;
+        public NoteApp()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataTable = new DataTable();
+            dataTable.Columns.Add("Title", typeof(string));
+            dataTable.Columns.Add("Messages", typeof(string));
+
+            MessageStore.DataSource= dataTable;
+
+            MessageStore.Columns["Messages"].Visible = false;
+
+            MessageStore.Columns["Title"].Width= 206;
+
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+                dataTable.Rows.Add(txtTitle.Text, txtMessage.Text);
+                txtTitle.Clear();
+                txtMessage.Clear();
+            
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            int index = MessageStore.CurrentCell.RowIndex;
+
+            if(index > -1)
+            {
+                txtTitle.Text = dataTable.Rows[index].ItemArray[0].ToString();
+                txtMessage.Text = dataTable.Rows[index].ItemArray[1].ToString();
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = MessageStore.CurrentCell.RowIndex;
+            dataTable.Rows[index ].Delete();
+            txtMessage.Text = "";
+            txtTitle.Text = "";
+        }
+
+        private void txtTitle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
